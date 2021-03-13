0257ff142205c48af533a1aeaaf7b36ba4da751a
“Dániel <faller.daniel3@gmail.com>
2021-03-13 17:43:05 +0100

    betegségek.py megnézi hogy egy adott betegség hányszor fordult elő

---
+++ betegségek.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv('output.csv')
+
+betegségSzámok = {}
+
+össz = len(df["Sorszám"])
+
+for sor in df["Alapbetegségek"]:
+    for betegség in sor.split(', '):
+        if betegség in betegségSzámok:
+            betegségSzámok[betegség] = betegségSzámok[betegség] + 1
+        else:
+            betegségSzámok[betegség] = 1
+
+rendezett = {k: v for k, v in sorted(betegségSzámok.items(), key=lambda item: item[1], reverse=True)} #megrendezem
+
+for i in rendezett:
+    if betegségSzámok[i] > 100:
+        print("{} betegség {} alkalommal ({:.2f} százalék)".format(i, betegségSzámok[i], betegségSzámok[i]/össz))
